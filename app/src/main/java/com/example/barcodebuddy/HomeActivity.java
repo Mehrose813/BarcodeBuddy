e860d4343854f3319a165f2ae52c7ebf780264d7
Rose <mehrose.shahbaz@gmail.com>
2025-01-04 22:10:39 +0500

    Home screen and fragment created

--- app/src/main/java/com/example/barcodebuddy/HomeActivity.java
+++ app/src/main/java/com/example/barcodebuddy/HomeActivity.java
@@ -1,12 +1,19 @@
 package com.example.barcodebuddy;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,10 +22,29 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        LinearLayout container = findViewById(R.id.container);
+        BottomNavigationView nav = findViewById(R.id.btm_nav);
+
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.home){
+                    loadFragment(new HomeFragment());
+                }
+                if(item.getItemId() == R.id.setting){
+                    loadFragment(new SettingFragment());
+                }
+                else{
+                    loadFragment(new UserFragment());
+                }
+                return false;
+            }
         });
+    }
+    private void loadFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container , fragment).commit();
+
     }
 }
