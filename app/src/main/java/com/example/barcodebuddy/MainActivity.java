758b5c5c45dfed601813f10aa812bf71ea60b087
aleena057634 <aleena057634@gmail.com>
2025-01-04 12:36:04 +0500

    Intent Passing

--- app/src/main/java/com/example/barcodebuddy/MainActivity.java
+++ app/src/main/java/com/example/barcodebuddy/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.barcodebuddy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +16,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent(MainActivity.this,MainActivity3.class);
-        startActivity(intent);
+        Button btn=findViewById(R.id.btn_1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
