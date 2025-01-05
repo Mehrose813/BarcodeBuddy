8008c9d49729a2f40a2b6e24fa54d946696ecbf3
Ifra-Ahtasham <ifraahtasham5@gmail.com>
2025-01-05 16:51:34 +0500

    not pull

--- app/src/main/java/com/example/barcodebuddy/HomeActivity.java
+++ app/src/main/java/com/example/barcodebuddy/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.example.barcodebuddy;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,7 +21,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
-
+Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,36 @@ public class HomeActivity extends AppCompatActivity {
 
         LinearLayout container = findViewById(R.id.container);
         BottomNavigationView nav = findViewById(R.id.btm_nav);
+        btnLogout = findViewById(R.id.btn_logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+                builder.setTitle("Logout Account");
+                builder.setMessage("Are you Sure");
+                builder.setCancelable(true);
+
+                AlertDialog alert = builder.create();
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(HomeActivity.this,SignInActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        alert.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
 
         nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -33,18 +68,21 @@ public class HomeActivity extends AppCompatActivity {
                 if(item.getItemId() == R.id.home){
                     loadFragment(new HomeFragment());
                 }
-                if(item.getItemId() == R.id.setting){
+                else if(item.getItemId() == R.id.setting){
                     loadFragment(new SettingFragment());
                 }
                 else{
                     loadFragment(new UserFragment());
                 }
-                return false;
+                return true;
             }
         });
     }
     private void loadFragment(Fragment fragment){
-        getSupportFragmentManager().beginTransaction().replace(R.id.container , fragment).commit();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container , fragment)
+                .commit();
 
     }
 }
