5eb916a9ca5d6cd403fa8b245ac020d4f1ffce33
Rose <mehrose.shahbaz@gmail.com>
2025-01-08 20:38:52 +0500

    product

---
+++ app/src/main/java/com/example/barcodebuddy/Product.java
@@ -0,0 +1,21 @@
+package com.example.barcodebuddy;
+
+public class Product {
+    String name,desc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
