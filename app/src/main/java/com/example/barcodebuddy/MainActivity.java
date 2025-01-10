3583a73849c5570bfbf48ac0b0850f662e55afe8
Ifra-Ahtasham <ifraahtasham5@gmail.com>
2025-01-10 13:35:56 +0500

    mainactivity_admin(check)+user(check))

--- app/src/main/java/com/example/barcodebuddy/MainActivity.java
+++ app/src/main/java/com/example/barcodebuddy/MainActivity.java
@@ -12,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 Button btn;
@@ -36,11 +41,55 @@ Button btn;
 
         }
 
-        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+        else {
             btn.setVisibility(View.GONE);
-            Intent intent = new Intent(MainActivity.this,HomeActivity.class);
-            startActivity(intent);
-            finish();
+
+            // Fetch user type from the database
+            String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+
+            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        String userType = snapshot.child("type").getValue(String.class);
+
+                        if ("admin".equals(userType)) {
+                            Intent intent = new Intent(MainActivity.this, AdminMainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    } else {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                        startActivity(intent);
+                        //finish();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                    startActivity(intent);
+                    //finish();
+                }
+            });
+
+        }
+
+
+//        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+//
+//                btn.setVisibility(View.GONE);
+//                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//                finish();
+
 
         }
 
@@ -51,4 +100,3 @@ Button btn;
 
 
     }
-}
