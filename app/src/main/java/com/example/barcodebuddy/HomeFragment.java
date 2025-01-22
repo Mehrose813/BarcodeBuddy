c1df6cbe1b7007aed5718d25648602b5dcf2fef9
Ifra-Ahtasham <ifraahtasham5@gmail.com>
2025-01-22 08:58:20 +0500

    userhome

--- app/src/main/java/com/example/barcodebuddy/HomeFragment.java
+++ app/src/main/java/com/example/barcodebuddy/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.barcodebuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,7 +16,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
+CardView searchCard;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +61,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        searchCard = view.findViewById(R.id.search_card);
+
+        searchCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(requireContext(),SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
